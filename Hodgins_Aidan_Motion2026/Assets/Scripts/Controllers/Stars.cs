7cfac5cebf8d502b189e005c10cb2a1db1cc9127
hodginai <hodginai@OA-S142-06>
2026-09-21 18:05:29 -0400

    im a dumbass

--- Hodgins_Aidan_Motion2026/Assets/Scripts/Controllers/Stars.cs
+++ Hodgins_Aidan_Motion2026/Assets/Scripts/Controllers/Stars.cs
@@ -7,8 +7,52 @@ public class Stars : MonoBehaviour
     public List<Transform> starTransforms;
     public float drawingTime;
 
+    public float time;
+
     // Update is called once per frame
     void Update()
     {
+        
+        DrawConstellations(starTransforms);
+
     }
-}
+
+
+
+    void DrawConstellations(List<Transform> starList)
+    {
+        time += Time.deltaTime;
+
+        for (int i = 0; i < starList.Count;)
+        {
+
+            if(i + 1 >= starList.Count)
+            {
+                break;
+            }
+            ///
+            ///
+            /// USE LERP DUMB BITCH ASS
+            ///
+
+            ///
+            Vector2 startingPoint = starList[i].position;
+
+            Vector2 endPoint = starList[i + 1].position;
+
+            Vector2 directionToStar = endPoint - startingPoint;
+
+            Vector2 magnitudeToStar = directionToStar * (time / drawingTime);
+
+            Debug.DrawLine(startingPoint, magnitudeToStar, Color.yellow);
+
+        Debug.Log("Drawing");
+        }
+
+
+        }
+
+
+
+    }
+
